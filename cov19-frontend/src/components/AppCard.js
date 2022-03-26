b4c04edbf1707eabbad9d4ab63dba934ea748a92
stevemooney123 <stephen@kaizenappz.com>
2022-03-26 20:06:46 +0000

    refactor components

--- cov19-frontend/src/components/AppCard.js
+++ cov19-frontend/src/components/AppCard.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react'
-import { Table, Card, Button } from 'react-bootstrap'
+import React, {Component} from 'react'
+import {Table, Card, Button} from 'react-bootstrap'
 
 export default class AppCard extends Component {
-  render() {
-    return (
-        <div>
-             
-      <Card style={{ width: '18rem', marginBottom: '1rem', display: 'flex', alignItems: 'center', alignContent: 'center', alignSelf: 'center' }}>
-  <Card.Body>
-    <Card.Title>{this.props.title}</Card.Title>
-    <Card.Text>
-    <Table striped bordered hover>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>{this.props.data}</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan={2}>Larry the Bird</td>
-    </tr>
-  </tbody>
-</Table>
-    </Card.Text>
-  </Card.Body>
-</Card>
-        </div>
- 
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            <Table striped bordered hover>
+                                <tbody>
+                                <tr>
+                                    <td><strong>{this.props.metricTitle}</strong></td>
+                                    <td>{this.props.value}</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Jacob</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td colSpan={2}>Larry the Bird</td>
+                                </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+
+        )
+    }
 }
--- cov19-frontend/src/components/Dashboard.js
+++ cov19-frontend/src/components/Dashboard.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import React, {Component} from 'react'
+import {Container, Row, Col, Card, Button} from 'react-bootstrap'
 import AppCard from './AppCard'
 import axios from 'axios'
 import configData from '../config/config.json'
+
 export default class Dashboard extends Component {
 
-  state = {
-    metrics: [],
-    lastSevenDays: 0,
-    totalCases: 0
-  }
+    state = {
+        metrics: []
+    }
 
-  componentDidMount() {
-    axios.get(configData.BASE_URL + `areaType=nation&areaName=Northern Ireland&page=1`)
-      .then(res => {
-        const metrics = res.data;
-       
-        this.setState({ metrics: res.data[0] });
-        const sum = [metrics[0].newCases, metrics[1].newCases, metrics[2].newCases, metrics[3].newCases, metrics[4].newCases, metrics[5].newCases, metrics[6].newCases].reduce((partialSum, a) => partialSum + a, 0);
-        this.setState({lastSevenDays: sum})
+    componentDidMount() {
+        axios.get(configData.BASE_URL + `last7?areaType=nation&areaName=Northern Ireland&metric=dailyCases&metricName=newCasesByPublishDate&page=11`)
+            .then(res => {
+                const metrics = "1234";
+                //console.log(metrics);
+                const info = {
+                    title: "Cases",
+                    key: "New Cases",
+                    value: metrics
+                }
 
-        var count = 0;
-   for(var i=0, n=metrics.length; i < n; i++) 
-   { 
-      count += metrics[i].newCases; 
-   }
-        this.setState({totalCases: count})
-      })
-  }
 
-  render() {
-    return (
-        <div>
+                this.state.metrics.push(info);
+                this.setState({metrics: this.state.metrics});
 
-<Container>
-<div>{this.state.lastSevenDays}</div>
-<div>{this.state.totalCases}</div>
-  <Row >
-    <Col md={3}> <AppCard title="Testing" data={this.state.metrics.newCases}/></Col>
-    <Col md={3}> <AppCard title="Inpatients" data={this.state.metrics.newCases}/></Col>
-    <Col md={3}> <AppCard title="Deaths" data={this.state.metrics.newCases}/></Col>
-    <Col md={3}> <AppCard title="Occupancy" data={this.state.metrics.newCases}/></Col>
-    <Col md={3}> <AppCard title="Outbreaks" data={this.state.metrics.newCases}/></Col>
-  </Row>
-  
-</Container>
-             
-    
-        </div>
- 
-    )
-  }
+                console.log(this.state.metrics);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+
+                <Container>
+                    <Row>
+                        {
+                            this.state.metrics.map((item, i) => (
+                            <span key={i}>
+                                <Col md={3}> <AppCard title={item.title} metricTitle={item.key} value={item.value}/></Col>
+                            </span>
+                        ))}
+
+                    </Row>
+
+                </Container>
+
+
+            </div>
+
+        )
+    }
 }
