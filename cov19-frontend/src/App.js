1eb1eaa411a19c58334c208e0bda19c7e958cdb7
stevemooney123 <stephen@kaizenappz.com>
2022-03-29 10:16:21 +0100

    update navbar

--- cov19-frontend/src/App.js
+++ cov19-frontend/src/App.js
@@ -14,19 +14,7 @@ function App() {
     return (
         <Router>
             <div>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="#home">Covid 19 Dashboard</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/">Dashboard</Nav.Link>
-                                <Nav.Link href="/symptoms">Symptoms</Nav.Link>
-                                <Nav.Link href="/book-test">Book Test</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+              <NavBar/>
 
                 {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
--- cov19-frontend/src/components/NavBar.js
+++ cov19-frontend/src/components/NavBar.js
@@ -1,33 +1,22 @@
+import {  Link } from "react-router-dom";
+import { Nav, NavDropdown, Button, Navbar } from 'react-bootstrap'
+import { Container } from "reactstrap";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import {Nav, Navbar} from "react-bootstrap";
-import {Container} from "reactstrap";
-
-export default function App() {
+const NavBar= () =>{
   return (
-      <Router>
-        <div>
-          <Navbar bg="light" expand="lg">
-            <Container>
-              <Navbar.Brand href="#home">Covid 19 Dashboard</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Dashboard</Nav.Link>
-                  <Nav.Link href="#link">Symptoms</Nav.Link>
-                  <Nav.Link href="#link">Book Test</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-
-        </div>
-      </Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Covid 19 Dashboard</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Dashboard</Nav.Link>
+              <Nav.Link href="/symptoms">Symptoms</Nav.Link>
+              <Nav.Link href="/book-test">Book Test</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
   );
 }
-
+export default NavBar;
